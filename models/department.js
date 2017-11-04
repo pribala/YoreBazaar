e08e1c9fd1a3aa6279f7816021125fb60d57d84e
bindu7772 <manubpatel4@gmail.com>
2017-11-04 13:29:33 -0400

    dept model

---
+++ models/department.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+  var Department = sequelize.define("Department", {
+    dept_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    overhead_costs: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    product_sale: {
+      type: DataTypes.DECIMAL ,
+      allowNull:false
+    }
+  });
+  return Post;
+};
