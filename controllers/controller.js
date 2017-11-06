d2678d69c4972f5f4ca91bb950c70424827500d9
kinnera01 <kinnera.bezawada1@gmail.com>
2017-11-06 15:14:59 -0500

    added iproducts

--- controllers/controller.js
+++ controllers/controller.js
@@ -15,11 +15,31 @@ var items = [
     Label: "chethan bhagath Novel",
     Price: "12$"
   }, 
+  {
+    image_url: "https://images.gr-assets.com/books/1470082995l/29056083.jpg",
+    Label: "The Cursed Child",
+    Price: "43$"
+  }, 
+  {
+    image_url: "https://wellcomebookprize.org/sites/default/files/styles/book_main/public/Kalanithi%20flat.png?itok=cBg6ptiC",
+    Label: "When Breath Becomes Air ",
+    Price: "28$"
+  }, 
+  {
+    image_url: "https://images-na.ssl-images-amazon.com/images/I/518ec-hoKML._SY445_QL70_.jpg",
+    Label: "Truly Madly Guilty ",
+    Price: "13$"
+  }, 
+  {
+    image_url: "https://i.pinimg.com/236x/96/c4/d2/96c4d2a64618ac79d13ffd6b4a6ba40d--lady-midnight-cassandra-clare-cassandra-clare-books.jpg",
+    Label: "Lady Midnight",
+    Price: "20$"
+  }, 
 ]
 //Create all our routes and set up logic within those routes where required.
 router.get('/', function (req, res, next) {
   // res.render("shop/index", { title: 'Shopping Cart' });
-  res.render("sample", { item: items });
+  res.render("shop/index", { item: items });
 });
 
 
