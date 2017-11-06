8fcc0c7f73bc220effb12012d3f2bfc84906fae0
kinnera01 <kinnera.bezawada1@gmail.com>
2017-11-06 01:44:58 -0500

    added handle bars index pages

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,13 +2,13 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-// var burgers = require("../models/burger.js");
+//Import the model (cat.js) to use its database functions.
+//var items = require("../models/index.js");
 
-// Create all our routes and set up logic within those routes where required.
-// router.get('/', function(req, res){
-// 	res.redirect('/burgers')
-// });
-
+//Create all our routes and set up logic within those routes where required.
+router.get('/', function(req, res, next) {
+    res.render("shop/index", { title: 'Shopping Cart' });
+  });
+  
 
 module.exports = router;
