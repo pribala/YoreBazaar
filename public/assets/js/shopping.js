63924b0a6c11185dec13c7fad2dc7e1bb8d705ee
Priya Balakrishnan <andu_pri@yahoo.com>
2017-11-13 23:36:53 -0500

    feat: Display selected profile name on navbar

--- public/assets/js/shopping.js
+++ public/assets/js/shopping.js
@@ -5,13 +5,15 @@ $(document).ready(function(){
 	$(".cart-btn").click(function(e){
 		e.preventDefault();
 		var prod_id = $(this).data("id");
-		var profile_id = $("#profile-id").text();
+		//var profile_id = $("#profile-id").text();
+		var profile_id = sessionStorage.getItem("profile-id");
 		var cartItem = {
 			product_id: prod_id,
 			profile_id: profile_id 
 		}
 		$.post("/addtocart", cartItem).then(function(data){
-			var profile_id = $("#profile-id").text();
+			//var profile_id = $("#profile-id").text();
+			console.log(profile_id);
 			window.location.replace("/cart/"+profile_id);
 		});
 	});    
