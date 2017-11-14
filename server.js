eed3168e4bae36639749acdaf5d39c60d7ad8027
kinnera01 <kinnera.bezawada1@gmail.com>
2017-11-14 18:27:33 -0500

    dsd

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var passport = require("./config/passport");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3030;
+var PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
 var db = require("./models");
