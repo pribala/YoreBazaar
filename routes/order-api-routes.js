eea2e7cd3a0ca9c1514d5902b8b83c74fbd6a128
kinnera01 <kinnera.bezawada1@gmail.com>
2017-11-16 18:32:31 -0500

    ss

--- routes/order-api-routes.js
+++ routes/order-api-routes.js
@@ -25,19 +25,7 @@
   
     // }
     app.get("/orders/", function(req, res) {
-      // findAll returns all entries for a table when used with no options
-      // var query = {};
-      // if (req.params.profile_id) {
-      //   query.ProfileProfileId = req.params.profile_id;
-      // }
-      // db.Order.findAll({
-      //     where: query,
-      //       include:[{model:db.Profile},
-      //        {model: db.Product}
-      //        ]
-      // }).then(function(dborder) {
-        //We have access to the products as an argument inside of the callback function
-     
+
       res.render("shop/orders");
       // });
     });
